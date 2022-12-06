02abe1f1e09c50acf398746201e83200e547188d
95tuanle <tlna95@icloud.com>
2022-12-05 23:47:20 -0500

    add header comments and jQuery

--- config/database.js
+++ config/database.js
@@ -1,3 +1,13 @@
+/******************************************************************************
+ * ITE5315 – Project
+ * I declare that this assignment is my own work in accordance with Humber Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ *  (including websites) or distributed to other students.
+ * Group members:
+ *  Nguyen Anh Tuan Le N01414195
+ *  Shubham Singh N01469289
+ * Date: December 6th, 2022
+ * *********************************************************************************/
 require('dotenv').config()
 module.exports = {
     url_atlas : `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DATABASE}?retryWrites=true&w=majority`
--- models/user.js
+++ models/user.js
@@ -1,3 +1,13 @@
+/******************************************************************************
+ * ITE5315 – Project
+ * I declare that this assignment is my own work in accordance with Humber Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ *  (including websites) or distributed to other students.
+ * Group members:
+ *  Nguyen Anh Tuan Le N01414195
+ *  Shubham Singh N01469289
+ * Date: December 6th, 2022
+ * *********************************************************************************/
 // load mongoose since we need it to define a model
 const mongoose = require('mongoose')
 const UserSchema = require('./schemas/user-schema')
